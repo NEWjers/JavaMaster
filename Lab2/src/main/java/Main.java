7d7224307b1abf33d6bc483c843a15f2738fbf32
NEWjers <53339526+NEWjers@users.noreply.github.com>
2022-03-25 12:03:39 +0200

    Delete Lab2 directory

--- Lab2/src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-import dao.CarDAO;
-
-public class Main {
-    public static void main(String[] args) {
-        CarDAO carDAO = new CarDAO();
-        System.out.println(carDAO.getById(1));
-    }
-}
